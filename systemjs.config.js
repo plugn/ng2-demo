2bccd838e89326e6b12714ebc29a1f366ff86a68
Maxim Dolgov <bananafishbone@gmail.com>
2016-09-01 17:55:36 +0300

    angular2-tree-component usage

--- systemjs.config.js
+++ systemjs.config.js
@@ -8,13 +8,18 @@
 		'app':                        'app', // 'dist',
 		'@angular':                   'node_modules/@angular',
 		'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
-		'rxjs':                       'node_modules/rxjs'
+		'rxjs':                       'node_modules/rxjs',
+		'angular2-tree-component':    'node_modules/angular2-tree-component',
+		'lodash':                     'node_modules/lodash'
+
 	};
 	// packages tells the System loader how to load when no filename and/or no extension
 	var packages = {
 		'app':                        { main: 'main.js',  defaultExtension: 'js' },
 		'rxjs':                       { defaultExtension: 'js' },
 		'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
+		'angular2-tree-component'   : { main: 'dist/angular2-tree-component.js', defaultExtension: 'js' },
+		'lodash'                    : { main: 'lodash.js', defaultExtension: 'js' }
 	};
 	var ngPackageNames = [
 		'common',
